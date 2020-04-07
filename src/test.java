6db0d89ced6a1dd35234ce8c1e2d4407dc417887
kun <life.restart@foxmail.com>
2020-04-07 16:32:54 +0800

    测试

---
+++ src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(i+"*"+j+"="+i*j+" ");
+                if (j==i){
+                    System.out.println();
+                }
+            }
+            }
+        }
+        }
+
